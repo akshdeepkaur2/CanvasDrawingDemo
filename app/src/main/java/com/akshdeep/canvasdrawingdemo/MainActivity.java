6dd1f37e001b256c05cd73ab2a98f85452ea1416
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-09-23 11:14:59 -0400

    Initial commit

---
+++ app/src/main/java/com/akshdeep/canvasdrawingdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.akshdeep.canvasdrawingdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Bitmap b  = Bitmap.createBitmap(300, 500, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(b);
+        Paint paintbrush = new Paint();
+        
+
+        // put te canvas into the frames
+        imageView.setImageBitmap(b);
+    }
+}
